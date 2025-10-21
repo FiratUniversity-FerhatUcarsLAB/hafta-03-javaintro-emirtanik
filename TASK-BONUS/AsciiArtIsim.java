918ecada5e66aa8e6ceca368db34cfc8f0a38d20
emirtanik <emirtanik5656@gmail.com>
2025-10-21 22:09:28 +0300

    Add AsciiArtIsim.java for ASCII art display

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,9 @@
+public class AsciiAd {
+    public static void main(String[] args) {
+        System.out.println("EEEEE  MM   MM  III  RRRR");
+        System.out.println("E      M M M M   I   R   R");
+        System.out.println("EEE    M  M  M   I   RRRR");
+        System.out.println("E      M     M   I   R R");
+        System.out.println("EEEEE  M     M  III  R  R");
+    }
+}
