5ec6a40ffe14638192aaffa731e5bf3cb33330ff
emirtanik <emirtanik5656@gmail.com>
2025-10-21 22:07:09 +0300

    Add multiplication table for the number 2

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,9 @@
+public class CarpimTablosu {
+    public static void main(String[] args) {
+        System.out.println("2'nin Çarpım Tablosu");
+        System.out.println("----------------------");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("2 x " + i + " = " + (2 * i));
+        }
+    }
+}
