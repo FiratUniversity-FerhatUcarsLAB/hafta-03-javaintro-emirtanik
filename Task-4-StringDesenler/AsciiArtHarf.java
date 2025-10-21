1c0deb56e0e2b677eefff108b00f3eaaf2644497
emirtanik <emirtanik5656@gmail.com>
2025-10-21 22:06:45 +0300

    Add AsciiHarf class to print letter 'E' in ASCII art

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,9 @@
+public class AsciiHarf {
+    public static void main(String[] args) {
+        System.out.println("EEEEEEE");
+        System.out.println("E");
+        System.out.println("EEE");
+        System.out.println("E");
+        System.out.println("EEEEEEE");
+    }
+}
